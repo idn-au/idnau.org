9c261a071869d85069366a7d01c17e0701c60e88
jamiefeiss <jamie.feiss@gmail.com>
2023-01-23 13:20:07 +1000

    Fixed 404 issues, added 404 page

--- src/router/index.js
+++ src/router/index.js
@@ -106,6 +106,11 @@ const router = createRouter({
         //         }
         //     }
         // }
+        {
+            path: "/:pathMatch(.*)*",
+            name: "not found",
+            component: () => import("@/views/NotFoundView.vue")
+        },
     ]
 });
 
