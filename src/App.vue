5166d27222d4ba5d40c99cccd3e7138425bb380d
jamiefeiss <jamie.feiss@gmail.com>
2023-01-23 17:17:58 +1000

    Updated Header links to cater for hashes

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <script setup>
 import { RouterView, RouterLink } from "vue-router";
 import { HeaderComponent, MainComponent, FooterComponent, navLinks } from "@idn-au/idn-lib";
+import idnLogo from "@/assets/images/idn-logo-250.png";
 
 const banners = [
     {
@@ -12,7 +13,19 @@ const banners = [
 
 <template>
     <HeaderComponent :internal="true" :banners="banners">
-        <RouterLink v-for="link in navLinks" :to="link.url">{{ link.label }}</RouterLink>
+        <template #logo-link>
+            <RouterLink id="idn-logo" to="/">
+                <img :src="idnLogo" alt="IDN Logo" />
+            </RouterLink>
+        </template>
+        <template #heading-link>
+            <RouterLink to="/">
+                <h2>IDN Catalogue Project</h2>
+            </RouterLink>
+        </template>
+        <template #links>
+            <RouterLink v-for="link in navLinks" :to="link.url">{{ link.label }}</RouterLink>
+        </template>
     </HeaderComponent>
     <MainComponent>
         <RouterView />
