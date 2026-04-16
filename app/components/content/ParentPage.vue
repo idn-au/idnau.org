cd5c139b21bf6c362c7f9e6b335d45a9356d9a39
jamiefeiss <jamie.feiss@gmail.com>
2026-04-16 12:53:22 +1000

    Added FAQ accordion component, added generic parent page rendering

---
+++ app/components/content/ParentPage.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+import type { ContentNavigationItem } from "@nuxt/content";
+import { findPageChildren } from "@nuxt/content/utils";
+
+const route = useRoute();
+
+const { data: navigation } = await useAsyncData(
+	"navigation",
+	() => queryCollectionNavigation("content", ["description", "websiteURL"]),
+	{
+		default: () => [] as ContentNavigationItem[],
+	}
+);
+
+const children = computed(() => findPageChildren(navigation.value, route.path));
+</script>
+
+<template>
+	<div v-for="child in children">
+		<h2 class="font-medium">{{child.title}}</h2>
+		<p>{{child.description}}</p>
+		<Button class="btn" asChild>
+			<NuxtLink :to="child.path">Read more</NuxtLink>
+		</Button>
+	</div>
+</template>
