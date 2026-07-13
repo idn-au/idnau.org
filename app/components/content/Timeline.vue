80a3eac1a8cc22b236307741c37cdc564e72d327
Casey-Haseloff <casey.j.haseloff@unimelb.edu.au>
2026-07-14 09:46:26 +1000

    Revert "Merge branch 'embedded_content' into updated_content_casey"
    This reverts commit 9bbbcd0172e5b3b98bb2d936e1667fc21e127b61, reversing
    changes made to 0d2214588fab70ee4c791fe30af92b325d2089c0.

--- app/components/content/Timeline.vue
+++ app/components/content/Timeline.vue
@@ -1,33 +1,18 @@
 <script setup lang="ts">
-import {Clock, MapPin} from "@lucide/vue";
-
 const props = defineProps<{
-	events: EventItem[];
-	highlightFirst?: boolean;
+	timeline: TimelineItem[];
 }>();
 </script>
 
 <template>
-	<div class="grid grid-cols-[min-content_1fr] sm:grid-cols-[10.5rem_min-content_1fr]">
-		<template v-for="(event, index) in props.events">
-			<div class="max-sm:hidden text-right p-2 flex flex-col shrink-0">
-				<div>{{formatDate(event.start)}}<template v-if="event.end"> - </template></div>
-				<div v-if="event.end">{{formatDate(event.end)}}</div>
-			</div>
-			<div class="relative">
-				<div class="h-full absolute left-1/2 -translate-x-1/2 bg-primary w-0.5 -z-1"></div>
-				<div :class="`mt-3.5 rounded-full aspect-square size-4 border-2 border-primary ${props.highlightFirst && index === 0 ? 'bg-primary' : 'bg-background'}`"></div>
-			</div>
-			<div class="p-2 mb-4">
-				<component :is="event.url ? 'a' : 'div'" :href="event.url" class="text-xl font-bold">{{event.title}}</component>
-				<div class="sm:hidden flex flex-row items-center gap-1">
-					<Clock class="size-4" /> {{formatDate(event.start)}} <template v-if="event.end"> - {{formatDate(event.end)}}</template>
-				</div>
-				<div v-if="event.location" class="flex flex-row items-center gap-1">
-					<MapPin class="size-4" /> {{event.location}}
-				</div>
-				<p v-if="event.description" class="line-clamp-3 text-ellipsis m-0!">{{event.description}}</p>
-			</div>
-		</template>
+	<div>
+		<div v-for="item in props.timeline" class="border-l flex flex-col relative p-2 pl-4">
+			<span class="rounded-full aspect-square size-4 bg-primary absolute -left-2 top-4"></span>
+			<span>{{formatDate(item.date)}}</span>
+			<span>{{item.title}}</span>
+			<p v-if="item.description" class="italic text-sm text-muted-foreground">
+				{{item.description}}
+			</p>
+		</div>
 	</div>
 </template>
